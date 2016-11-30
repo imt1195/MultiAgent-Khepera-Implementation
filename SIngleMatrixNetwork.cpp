df551d4176d7527518682f1d6117bbfaf4fc4ad9
Ian Tobin <imt1195@gmail.com>
2016-11-30 15:34:51 -0500

    Add files via upload
    started updating SingleMatrixNetwork.cpp to create artificial neural network for Khepera II robot

--- SIngleMatrixNetwork.cpp
+++ SIngleMatrixNetwork.cpp
@@ -10,75 +10,36 @@ int main(int argc, char* argv[])
 {
 	double input[6];
 	int i;
-	//int j;
+	
+	Network khepera(4,12,2,"Ian_Tobin__Test1.txt");
 
-//	Network ted(3,10,2,"blank_network.txt");
+	khepera.PrintNetworkState();
 
-//	Network fred;
-	Network fred("antJaw_05.txt");
-
-	fred.PrintNetworkState();
-
-
-	// Initialize the weights to produce a test recurrent network
-/*
-	fred.setNetworkWeights(0.0);					// initialize all connections to zero
-	fred.setNetworkWeightsRectangle(0.3,3,7,0,3);	// set feedforward connections from inputs to interneurons 
-	fred.setNetworkWeightsRectangle(0.5,7,9,3,7);	// set feedforward connections from interneurons to output neurons 
-	fred.setNetworkWeightsRectangle(-0.25,3,7,3,7);	// set recurrent connections from interneurons to interneurons
-	fred.setNetworkWeightsDiagonalRange(0.1,0,9);	// set network autapses
-  */
-
-/*	
-	//--- Identity Matrix ----
-	fred.setNetworkWeights(0.0);
-	fred.setNetworkWeightsDiagonalRange(1,0,8);
-*/
-
-	fred.writeNetworkOutputStateToFile( "testOutput10.txt" );
+	khepera.writeNetworkOutputStateToFile("Ian_Tobin_OutputTest1.txt");
 
 	i = 0;
-	while( i < 1000 ){
-
-		
-		input[0] = input[1] = input[2]	= 0.0;			// Set all inputs off
-		if( i> 300 && i < 700)	input[0] = 1.0;		// Command jaw close 
-		if( i> 600 && i < 800) input[1] = 1.0;		// command LEFT jaw open
-		if( i> 650 && i < 850) input[2] = 1.0;		// command RIGHT jaw open
-		// Vary duration of command 
-		// Get offset to work - correct resting state
-		// close and then sensory input
-		// approach close, then before it's closed get sensory input to see if it stops
+	while (i < 1000) {
 
 
+		input[0] = input[1] = input[2] = 0.0;			// Set all inputs off
+		if (i> 300 && i < 700)	input[0] = 1.0;		// Command jaw close 
+		if (i> 600 && i < 800) input[1] = 1.0;		// command LEFT jaw open
+		if (i> 650 && i < 850) input[2] = 1.0;		// command RIGHT jaw open
+													// Vary duration of command 
+													// Get offset to work - correct resting state
+													// close and then sensory input
+													// approach close, then before it's closed get sensory input to see if it stops
 
-/*
-		//-----------------------------------------------------------
-		for(j = 0; j < 6; ++j ){	// temporary input function (STEP RESPONSE)
-			input[j]  =  sin((i + j*3)*0.01*i);
-//			input[j]  = 1.0;
-		}
-		//---------------------------------------------------------------------
-*/
-		fred.setNetworkInput( input );
 
-		fred.cycleNetwork();
 
-//		fred.cycleNetworkNormalizeHebbianLearning();
+		khepera.setNetworkInput(input);
 
-//		fred.printNetworkOuput();
-		fred.printNetworkOutputState( );
+		khepera.cycleNetwork();
+		khepera.printNetworkOutputState();
 
-		fred.writeNetworkOutputStateToFile( "testOutput10.txt" );
-
-//		fred.writeNetworkToFile("test.txt");
-	//	fred.writeNetworkWeightsToFile("weights.txt");
+		khepera.writeNetworkOutputStateToFile("Ian_Tobin_OutputTest1.txt");
 		++i;
 	}
-
-	//Test the read-write fucntions -------------
-//	fred.writeNetworkToFile("temp.txt");
-//	fred.readNetworkFromFile("temp.txt");
 
 	return 0;
 }
