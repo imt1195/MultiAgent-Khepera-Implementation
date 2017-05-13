a8e95af0e51fda13b0707c746c601a8526abb971
Ian Tobin <imt1195@gmail.com>
2017-05-13 15:41:44 -0400

    Add files via upload
    All inputs and outputs I have used and gotten while working on this projects starts start with my name.
    Ex.     Ian_Test_ ... _Inputs.txt          Ian_Test_ ... _Outputs.txt

--- SIngleMatrixNetwork.cpp
+++ SIngleMatrixNetwork.cpp
@@ -8,38 +8,47 @@
 
 int main(int argc, char* argv[])
 {
-	double input[6];
-	int i;
-	
-	Network khepera(4,12,2,"Ian_Tobin__Test1.txt");
+	Network khepera("Ian_Test_Network_Changing_Inputs.txt"); //Ian_Test_Network_Equal_Vary_Inputs.txt
 
-	khepera.PrintNetworkState();
-
-	khepera.writeNetworkOutputStateToFile("Ian_Tobin_OutputTest1.txt");
-
-	i = 0;
-	while (i < 1000) {
-
-
-		input[0] = input[1] = input[2] = 0.0;			// Set all inputs off
-		if (i> 300 && i < 700)	input[0] = 1.0;		// Command jaw close 
-		if (i> 600 && i < 800) input[1] = 1.0;		// command LEFT jaw open
-		if (i> 650 && i < 850) input[2] = 1.0;		// command RIGHT jaw open
-													// Vary duration of command 
-													// Get offset to work - correct resting state
-													// close and then sensory input
-													// approach close, then before it's closed get sensory input to see if it stops
-
-
-
-		khepera.setNetworkInput(input);
+	int i = 0;
+	double input[7];
+	while (i < 710) {
 
 		khepera.cycleNetwork();
 		khepera.printNetworkOutputState();
 
-		khepera.writeNetworkOutputStateToFile("Ian_Tobin_OutputTest1.txt");
+		khepera.writeNetworkOutputStateToFile("Ian_Changing_Inputs_Output_v7.txt");
+
+		/*
+		if (i >= 201) {
+			input[0] = input[1] = input[2] = i - 200;
+			input[3] = input[4] = input[5] = 455 - input[0];
+			input[6] = 1;
+			khepera.setNetworkInput(input);
+		}
+		if (i >= 456) {
+			input[0] = input[1] = input[2] = 455 - input[0];
+			input[3] = input[4] = input[5] = i - 200;
+			input[6] = 1;
+			khepera.setNetworkInput(input);
+		}
+		*/
+		
+		
+		if (i >= 200) {
+			input[0] = input[1] = input[2] = 1.0;
+			input[3] = input[4] = input[5] = 0.5;
+			khepera.setNetworkInput(input);
+		}
+
+		if (i >= 400) {
+			input[0] = input[1] = input[2] = 0.5;
+			input[3] = input[4] = input[5] = 1.0;
+			khepera.setNetworkInput(input);
+		}
 		++i;
-	}
+	}	
 
 	return 0;
+
 }
